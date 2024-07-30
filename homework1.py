f6741baad9da6fe858d9fa4007504fc6bbd3c475
Anatoly <anatoly-1905@yandex.ru>
2024-07-30 23:23:01 +0300

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,5 @@
+homeworks = 12
+hours = 1.5
+course_name = 'Pithon'
+time_for_one_task = homeworks / hours
+print(course_name,Python,'всего задач: ',homeworks, 'затрачено часов: ', hours, 'среднее время выполнения ', 0.125 часа.)
